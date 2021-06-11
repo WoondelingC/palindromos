c51984d536997f4f2b992a42fae9b6c5bb91b7b2
Woondeling Colina <josecolinamartinez@gmail.com>
2021-06-10 22:40:07 -0500

    .

---
+++ palindromo.js
@@ -0,0 +1,17 @@
+function palindrome(str) {
+    let letras = /[\W_]/g;
+    
+    var letrasMinusculas = str.toLowerCase().replace(letras,"");
+    
+    let reversa= smallStr.split("").reverse().join("");
+    
+    if(reversa===smallStr)
+    return true;
+    
+    return false;
+    }
+    
+
+    palindrome("eye");
+
+    document.querySelector(#).innertHTML = reversa
